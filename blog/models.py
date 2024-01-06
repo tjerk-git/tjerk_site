88b70cd854165ca098f7cab8381ff845ea7bdae0
Tjerk Dijkstra <tjerk.dijkstra@icloud.com>
2024-01-06 13:38:52 +0100

    added models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=100)
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email_address = models.EmailField()
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    excerpt = models.TextField(max_length=200)
+    image_name = models.CharField(max_length=100)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(default="", null=False, unique=True)
+    content = models.TextField()
+    author = models.ForeignKey(
+        Author, on_delete=models.SET_NULL, related_name="posts")
+    tags = models.ManyToManyField(Tag, verbose_name=("tags"))
