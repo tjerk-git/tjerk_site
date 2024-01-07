b14c1b2aa6a12772ee52fda822ea7170dee01d28
Tjerk Dijkstra <tjerk.dijkstra@icloud.com>
2024-01-07 20:25:12 +0100

    yeah

--- blog/admin.py
+++ blog/admin.py
@@ -3,6 +3,16 @@ from django.contrib import admin
 from .models import Post, Author, Tag
 # Register your models here.
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    # fields = ("title", "author", "excerpt", "content", "slug", "image_name", "date", "tags")
+    # exclude = ("date",)
+    list_display = ("title", "date", "author")
+    list_filter = ("author", "tags", "date")
+    prepopulated_fields = {"slug": ("title",)}
+    search_fields = ("title", "content")
+
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
 admin.site.register(Tag)
--- blog/models.py
+++ blog/models.py
@@ -6,11 +6,17 @@ from django.db import models
 class Tag(models.Model):
     caption = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.caption
+
 
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email_address = models.EmailField()
+
+    def __str__(self):
+        return self.first_name
 
 
 class Post(models.Model):
@@ -18,8 +24,12 @@ class Post(models.Model):
     excerpt = models.TextField(max_length=200)
     image_name = models.CharField(max_length=100)
     date = models.DateField(auto_now=True)
-    slug = models.SlugField(default="", null=False, unique=True)
+    slug = models.SlugField(default="", null=False,
+                            unique=True, editable=False)
     content = models.TextField()
     author = models.ForeignKey(
-        Author, on_delete=models.SET_NULL, related_name="posts")
+        Author, on_delete=models.SET_NULL, related_name="posts", null=True)
     tags = models.ManyToManyField(Tag, verbose_name=("tags"))
+
+    def __str__(self):
+        return self.title
